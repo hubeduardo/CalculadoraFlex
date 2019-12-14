1b793929a1861abc4871a012e07e7ede87b7e70c
Logon Aluno <logonaluno@local.com>
2019-12-14 11:14:55 -0300

    Initial commit

---
+++ app/src/main/java/br/com/ferreira/calculadoraflex/ui/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package br.com.ferreira.calculadoraflex.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.ferreira.calculadoraflex.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/br/com/ferreira/calculadoraflex/ui/signup/SignupActivity.kt
@@ -0,0 +1,11 @@
+package br.com.ferreira.calculadoraflex.ui.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
